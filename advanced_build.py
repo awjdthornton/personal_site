0e0c7eecbdbcd70f1424588a52168ff6b86f5330
Andrew Thornton <awjdthornton@gmail.com>
2019-06-20 12:25:38 -0700

    advanced pythong ssg complete

--- advanced_build.py
+++ advanced_build.py
@@ -11,13 +11,15 @@ template = Template(template)
 
 #combine template and content, replacing the page specific stuff in the template portion
 index_content = open('./content/index.html').read()
-index_page = template.safe_substitute(title="About Me", active="active", content=index_content, bg_class="bio")
+index_page = template.safe_substitute(title="About Me", active_bio="active", content=index_content, bg_class="bio")
 open('./docs/index.html' , 'w+').write(index_page)
 
-#blog_content = open('./content/blog.html').read()
-#open('./docs/blog.html' , 'w+').write(top + blog + bottom)
+blog_content = open('./content/blog.html').read()
+blog_page = template.safe_substitute(title="Blog", active_blog="active", content=blog_content, bg_class="blog")
+open('./docs/blog.html' , 'w+').write(blog_page)
 
-#product_ideas_content = open('./content/product_ideas.html').read()
-#open('./docs/product_ideas.html' , 'w+').write(top + product_ideas + bottom)
+product_ideas_content = open('./content/product_ideas.html').read()
+product_ideas_page = template.safe_substitute(title="Ideas", active_product_ideas="active", content=product_ideas_content, bg_class="product_ideas")
+open('./docs/product_ideas.html' , 'w+').write(product_ideas_page)
 
 print("advanced_build.py complete")
