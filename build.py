3a54529756c291bee2ec69992bd1b3d3cb3651f0
Andrew Thornton <awjdthornton@gmail.com>
2019-06-25 15:03:07 -0700

    getting rid of old .sh and .py build scripts and supporting templates

--- build.py
+++ build.py
@@ -1,14 +1,25 @@
+#trying to generate sites, set page title, and set active class all in one step
+
+#bring in the Template string methods
+from string import Template
+
 print("beginning build.py")
-top = open('./templates/top.html').read()
-bottom = open('./templates/bottom.html').read()
 
-index = open('./content/index.html').read()
-open('./docs/index.html' , 'w+').write(top + index + bottom)
+#open the advanced template and define it as a Template class
+template = open('./templates/template.html').read()
+template = Template(template)
 
-blog = open('./content/blog.html').read()
-open('./docs/blog.html' , 'w+').write(top + blog + bottom)
+#combine template and content, replacing the page specific stuff in the template portion
+index_content = open('./content/index.html').read()
+index_page = template.safe_substitute(title="About Me", active_bio="active", content=index_content, bg_class="bio")
+open('./docs/index.html' , 'w+').write(index_page)
 
-product_ideas = open('./content/product_ideas.html').read()
-open('./docs/product_ideas.html' , 'w+').write(top + product_ideas + bottom)
+blog_content = open('./content/blog.html').read()
+blog_page = template.safe_substitute(title="Blog", active_blog="active", content=blog_content, bg_class="blog")
+open('./docs/blog.html' , 'w+').write(blog_page)
+
+product_ideas_content = open('./content/product_ideas.html').read()
+product_ideas_page = template.safe_substitute(title="Ideas", active_product_ideas="active", content=product_ideas_content, bg_class="product_ideas")
+open('./docs/product_ideas.html' , 'w+').write(product_ideas_page)
 
 print("build.py complete")
