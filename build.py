da625c0eb97cd02626cbffba7671aa133bc4fd47
Andrew Thornton <awjdthornton@gmail.com>
2019-06-26 14:46:33 -0700

    changing ssg to use a for loop to generate each page in the pages list

--- build.py
+++ build.py
@@ -10,43 +10,35 @@ def main():
 		{
 			"filename": "./content/index.html",
 			"output_file": "./docs/index.html",
-			"title": "About Me",
-			"active_class": "active_bio",
+			"title": "Bio",
+			"active_bio": "active",
 			"bg_class": "bio",
 		},
 		{
 			"filename": "./content/blog.html",
 			"output_file": "./docs/blog.html",
 			"title": "Blog",
-			"active_class": "active_blog",
+			"active_blog": "active",
 			"bg_class": "blog",
 		},
 		{
 			"filename": "./content/product_ideas.html",
 			"output_file": "./docs/product_ideas.html",
 			"title": "Ideas",
-			"active_class": "active_product_ideas",
+			"active_product_ideas": "active",
 			"bg_class": "product_ideas",
 		},
 	]				
 			
-
 	#open the template and define it as a Template object
 	template = open('./templates/template.html').read()
 	template = Template(template)
-
-	#combine template and content, replacing the page specific stuff 
-	index_content = open('./content/index.html').read()
-	index_page = template.safe_substitute(title="About Me", active_bio="active", content=index_content, bg_class="bio")
-	open('./docs/index.html' , 'w+').write(index_page)
-
-	blog_content = open('./content/blog.html').read()
-	blog_page = template.safe_substitute(title="Blog", active_blog="active", content=blog_content, bg_class="blog")
-	open('./docs/blog.html' , 'w+').write(blog_page)
-
-	product_ideas_content = open('./content/product_ideas.html').read()
-	product_ideas_page = template.safe_substitute(title="Ideas", active_product_ideas="active", content=product_ideas_content, bg_class="product_ideas")
-	open('./docs/product_ideas.html' , 'w+').write(product_ideas_page)
+					
+	for page in pages:
+		#combine template and content, replacing the page specific stuff 
+		page_content = open(page["filename"]).read()
+		page_output = template.safe_substitute(page, content=page_content)
+		open(page["output_file"] , 'w+').write(page_output)
 
 	print("build.py static site generator - run complete")
 
