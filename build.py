0384180b4418d0e287820106f82215f556924102
Andrew Thornton <awjdthornton@gmail.com>
2019-06-26 13:29:37 -0700

    adding pages list with a dict for each page including all variables required for ssg

--- build.py
+++ build.py
@@ -4,7 +4,32 @@ def main():
 #bring in the Template string methods
 	from string import Template
 
-	print("beginning build.py")
+	print("build.py static site generator - run beginning")
+	
+	pages = [
+		{
+			"filename": "./content/index.html",
+			"output_file": "./docs/index.html",
+			"title": "About Me",
+			"active": "active_bio",
+			"bg_class": "bio",
+		},
+		{
+			"filename": "./content/blog.html",
+			"output_file": "./docs/blog.html",
+			"title": "Blog",
+			"active": "active_blog",
+			"bg_class": "blog",
+		},
+		{
+			"filename": "./content/product_ideas.html",
+			"output_file": "./docs/product_ideas.html",
+			"title": "Ideas",
+			"active": "active_product_ideas",
+			"bg_class": "product_ideas",
+		},
+	]				
+			
 
 	#open the template and define it as a Template object
 	template = open('./templates/template.html').read()
@@ -23,7 +48,7 @@ def main():
 	product_ideas_page = template.safe_substitute(title="Ideas", active_product_ideas="active", content=product_ideas_content, bg_class="product_ideas")
 	open('./docs/product_ideas.html' , 'w+').write(product_ideas_page)
 
-	print("build.py complete")
+	print("build.py static site generator - run complete")
 
 
 if __name__ == "__main__":
