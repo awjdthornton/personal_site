f6805019dfceb463d1affe533339eef87e652b5d
Andrew Thornton <awjdthornton@gmail.com>
2019-06-27 00:19:57 -0700

    factoring functions out of main()

--- build.py
+++ build.py
@@ -1,52 +1,54 @@
 #trying to generate sites, set page title, and set active class all in one step
 
-def main():
 #bring in the Template string methods
-	from string import Template
+from string import Template
+
+main_pages = [
+	{
+		"filename": "./content/index.html",
+		"output_file": "./docs/index.html",
+		"title": "Bio",
+		"active_bio": "active",
+		"bg_class": "bio",
+	},
+	{
+		"filename": "./content/blog.html",
+		"output_file": "./docs/blog.html",
+		"title": "Blog",
+		"active_blog": "active",
+		"bg_class": "blog",
+	},
+	{
+		"filename": "./content/product_ideas.html",
+		"output_file": "./docs/product_ideas.html",
+		"title": "Ideas",
+		"active_product_ideas": "active",
+		"bg_class": "product_ideas",
+	},
+]
+
+
+def open_template():		
+	#open the template and define it as a Template object
+	template = open('./templates/template.html').read()
+	template = Template(template)
+	return template
+
+
+def create_main_pages (template):
+	for page in main_pages:
+		#combine template and content, replacing the page specific stuff 
+		page_content = open(page["filename"]).read()
+		page_output = template.safe_substitute(page, content=page_content)
+		open(page["output_file"] , 'w+').write(page_output)
+
+
+def main():
 
 	print("build.py static site generator - run beginning")
 	
-	pages = [
-		{
-			"filename": "./content/index.html",
-			"output_file": "./docs/index.html",
-			"title": "Bio",
-			"active_bio": "active",
-			"bg_class": "bio",
-		},
-		{
-			"filename": "./content/blog.html",
-			"output_file": "./docs/blog.html",
-			"title": "Blog",
-			"active_blog": "active",
-			"bg_class": "blog",
-		},
-		{
-			"filename": "./content/product_ideas.html",
-			"output_file": "./docs/product_ideas.html",
-			"title": "Ideas",
-			"active_product_ideas": "active",
-			"bg_class": "product_ideas",
-		},
-	]				
-			
-	def open_template():		
-		#open the template and define it as a Template object
-		template = open('./templates/template.html').read()
-		template = Template(template)
-		return template
-					
-	
-	def create_pages (template):
-		for page in pages:
-			#combine template and content, replacing the page specific stuff 
-			page_content = open(page["filename"]).read()
-			page_output = template.safe_substitute(page, content=page_content)
-			open(page["output_file"] , 'w+').write(page_output)
-			
-	
 	tmpl = open_template()
-	create_pages(tmpl)
+	create_main_pages(tmpl)
 
 	print("build.py static site generator - run complete")
 
