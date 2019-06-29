9d4a3333d48069de824d0313a24368b01f182b8f
Andrew Thornton <awjdthornton@gmail.com>
2019-06-28 18:16:19 -0700

    making nav links auto-generate including active class for own page and turning <a> to <span>

--- build.py
+++ build.py
@@ -8,21 +8,24 @@ main_pages = [
 		"filename": "./content/index.html",
 		"output_file": "./docs/index.html",
 		"title": "Bio",
-		"active_bio": "active",
+		"href": "./index.html",
+		"link_label": "Bio",
 		"bg_class": "bio",
 	},
 	{
 		"filename": "./content/blog.html",
 		"output_file": "./docs/blog.html",
 		"title": "Blog",
-		"active_blog": "active",
+		"href": "./blog.html",
+		"link_label": "Blog",
 		"bg_class": "blog",
 	},
 	{
 		"filename": "./content/product_ideas.html",
 		"output_file": "./docs/product_ideas.html",
 		"title": "Ideas",
-		"active_product_ideas": "active",
+		"href": "./product_ideas.html",
+		"link_label": "Ideas",
 		"bg_class": "product_ideas",
 	},
 ]
@@ -32,14 +35,26 @@ def open_template():
 	#open the template and define it as a Template object
 	template = open('./templates/template.html').read()
 	template = Template(template)
+	nav = nav_links()
+	template = template.safe_substitute(nav_section=nav)	
 	return template
-
+	
+def nav_links():
+	#create nav links by looping through each page in the main_pages list
+	top_links = ''
+	for page in main_pages:
+		top_links = top_links + '\n' + '''					<li class="nav-item">
+						<a class="nav-link" href="'''+ page["href"] + '">' + page["link_label"] + '''</a>
+					</li>'''
+	return top_links
 
 def create_main_pages (template):
 	for page in main_pages:
 		#combine template and content, replacing the page specific stuff 
 		page_content = open(page["filename"]).read()
-		page_output = template.safe_substitute(page, content=page_content)
+		template = Template(template)
+		template = template.safe_substitute(page, content=page_content)
+		page_output = template.replace('<a class="nav-link" href="' + page['href'] + '">' + page['link_label'] + '</a>', '<span class="nav-link active" href="' + page['href'] + '">' + page['link_label'] + '</span>')
 		open(page["output_file"] , 'w+').write(page_output)
 
 
